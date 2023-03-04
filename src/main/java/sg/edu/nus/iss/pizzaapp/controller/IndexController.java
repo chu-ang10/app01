2fd74f75d3a9dfdebab48bd2981bd432a7e5bcf0
chanel <chanelhuang.xn@gmail.com>
2023-03-04 20:58:13 +0800

    app01 day1

---
+++ src/main/java/sg/edu/nus/iss/pizzaapp/controller/IndexController.java
@@ -0,0 +1,18 @@
+package sg.edu.nus.iss.pizzaapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+@GetMapping
+public String landingPage(Model model){
+    return "view0";
+}
+
+
+}
